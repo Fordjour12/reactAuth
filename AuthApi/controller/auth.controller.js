dde17b91058238366e5a34302f377d06fd0f7b8d
McCamble Kofi Fordjour Bobie <fordjourbobiem@gmail.com>
2021-12-05 19:00:49 +0000

    bcrypt hashing completed

--- AuthApi/controller/auth.controller.js
+++ AuthApi/controller/auth.controller.js
@@ -2,17 +2,32 @@ const { validationResult } = require('express-validator')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-
 // folder Declearation
-const userModel = require('../model/user.model')
+const userData = require('../model/user.model')
 
-
-
-exports.signup=(request,response,next)=>{
+exports.signup = (request, response, next) => {
 	const errors = validationResult(request)
-	
-	if(!errors.isEmpty()){
-		const error = new 
-	}
-}
 
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation Invalid')
+		error.statusCode = 422
+		error.data = error.array()
+		throw error
+	}
+
+	const username = request.body.username
+	const email = request.body.email
+	const password = request.body.password
+
+	bcrypt
+		.hash(password, 16)
+		.then((hashedPassword) => {
+			const user = new userData({
+				username: username,
+				email: email,
+				password: hashedPassword,
+			})
+			return user.save()
+		})
+		.then(result)
+}
